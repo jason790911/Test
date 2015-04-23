594752182923903ebe4a050f4e9feea75a56e9c9
jason790911 <jason790911@yahoo.com.tw>
2015-04-23 14:28:50 +0800

    Homework 2 about C#

---
+++ Hw2/Hw2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hw2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 8; ++i) {
+                for (int j = 0; j < 8; ++j) { 
+                    Console.Write((i+j)%2==0?"X":"O");
+                }
+                Console.WriteLine();
+            }
+            Console.Write("\nPress any key to continue... ");
+            Console.ReadLine();
+        }
+    }
+}
